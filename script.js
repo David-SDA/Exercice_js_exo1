cf495c6d7ded4a3879f0ecf9392aaefe7069631f
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-27 11:10:23 +0200

    Ajout du js, alert de propriété css

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-function getCSS(){
-    ;
+/* Fonction pour obtenir les propriétés css d'un élément */
+function getCSS(element){
+    let style = getComputedStyle(element);
+    return style;
 }
 
+/* Fonction pour faire des actions quand la boîte est cliqué */
 function boiteClique(){
-    alert("yo");
+    style = getCSS(boite);
+    let backgroundColor = style.backgroundColor;
+    let color = style.color;
+    let height = boite.offsetHeight;
+    let width = boite.offsetWidth;
+    let display = style.display;
+    let font = style.fontFamily + "(" + style.fontSize + ")";
+    alert("Class : " + boite.className + "\n" + "- Background color : " + backgroundColor + "\n- Color : " + color + "\n- Height : " + height + "\n- Width : " + width + "\n- Display : " + display + "\n- Display : " + font);
 }
 
-let boite = document.querySelector(".boite");
+/* On enregistre le carré créé */
+let boite = document.querySelector(".carre");
 
+/* Si le carré est cliqué, on applique la fonction en conséquence */
 boite.addEventListener("click", boiteClique);
